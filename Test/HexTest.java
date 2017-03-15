6484de1c7d11df494078f0dd5ed2f4b3adc95f02
snewsome <shawnmnewsome@ufl.edu>
2017-03-15 12:36:14 -0400

    Create HexTest.java

---
+++ Test/HexTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by NotKali on 3/14/2017.
+ */
+public class HexTest {
+    private Hex.Feature[] features = {Hex.Feature.VOLCANO,Hex.Feature.DESERT, Hex.Feature.PLAIN};
+    int [] sidesConnected = {0,0,0,0,0,1};
+
+    Hex firstTile = new Hex(sidesConnected, features);
+
+    @Test
+    public void getSidesConnected() throws Exception {
+        assertArrayEquals(sidesConnected,firstTile.getSidesConnected());
+    }
+
+    @Test
+    public void testConnected() throws Exception {
+        assertTrue(firstTile.isConnected());
+    }
+
+    @Test
+    public void testFeatures() throws Exception {
+        assertTrue(firstTile.hasFeatures());
+    }
+}
+
