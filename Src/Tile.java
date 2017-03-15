4adcd5a91ecc4b40976a5b0ea53d55439a0f2e77
snewsome <shawnmnewsome@ufl.edu>
2017-03-15 15:35:15 -0400

    Update Tile.java

--- Src/Tile.java
+++ Src/Tile.java
@@ -2,22 +2,13 @@
  * Created by NotKali on 3/15/2017.
  */
 public class Tile {
-    private Hex [] threeHexes;
+    private Hex [] ownedHexes;
 
-    public Tile(Hex[] threeHexes) {
-        this.threeHexes = threeHexes;
-    }
-
-    public Hex[] getThreeHexes() {
-        return threeHexes;
+    public Tile(Hex[] tileHexes) {
+        this.ownedHexes = tileHexes;
     }
 
     public boolean hasThreeHexes () {
-        if(threeHexes.length != 3) {
-            return false;
-        }
-        else {
-            return true;
-        }
+        return (ownedHexes.length == 3);
     }
 }
