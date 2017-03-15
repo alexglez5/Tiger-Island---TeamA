28f7ff52aa7f637b0bb60df336ed0c3cf3ddcbd4
snewsome <shawnmnewsome@ufl.edu>
2017-03-15 12:35:24 -0400

    Create Hex.java

---
+++ Src/Hex.java
@@ -0,0 +1,40 @@
+/**
+ * Created by NotKali on 3/8/2017.
+ */
+
+public class Hex {
+
+    private int [] sidesConnected;
+    public enum Feature{
+        VOLCANO, DESERT, WATER, JUNGLE, PLAIN;
+    }
+
+
+    private Feature [] features;
+
+    public Hex(int[] sidesConnected, Feature[] features) {
+        this.sidesConnected = sidesConnected;
+        this.features = features;
+    }
+
+    public int[] getSidesConnected() {
+        return sidesConnected;
+    }
+
+    public boolean isConnected() {
+        for(int i = 0; i < sidesConnected.length; i++){
+            if(sidesConnected[i]==1){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasFeatures() {
+        if(features.length!=0){
+            return true;
+        }
+        return false;
+    }
+
+}
