10f7cac68df24510c5456f0641f1bd9a2dc4b37f
24vincenti <vincentibarrola@Vincents-MacBook-Pro.local>
2017-03-15 18:13:12 -0400

    Player Class

--- Src/Graph.java
+++ Src/Graph.java
@@ -1,11 +1,11 @@
 /**
  * Created by NotKali on 3/15/2017.
  */
-public class Graph {
-    Hex [] [] board;
+public class Graph{
+    Hex [][] board;
     int horizontalLength;
 
-    public void changeTileIdentifier(int x, int y) {
+    public void changeTileIdentifier(int x, int y){
         Hex hexToBeChanged = board[x][y];
     }
 }
