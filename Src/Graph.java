28392400df398a12515dc2a174914938a6cc3b55
snewsome <shawnmnewsome@ufl.edu>
2017-03-15 18:42:19 -0400

    Adding Tile Identifier Implementation

--- Src/Graph.java
+++ Src/Graph.java
@@ -4,16 +4,20 @@
 public class Graph {
     int horizontalLength = 10;
     int verticalLength = 10;
-    Hex [] [] hexes = new Hex[horizontalLength][verticalLength];
+    Hex [] [] board = new Hex[horizontalLength][verticalLength];
 
     public Graph() {
     }
 
     public void addHex(int x, int y) {
-        hexes[x][y] = new Hex(Hex.TerrainType.VOLCANO);
+        board[x][y] = new Hex(Hex.TerrainType.VOLCANO);
     }
 
     public boolean hexExists(int x, int y){
-        return(hexes[x][y]!=null);
+        return(board[x][y]!=null);
+    }
+
+    public void changeTileIdentifer(int x, int y, int tileID_number) {
+        board[x][y].setTileIdentifier(tileID_number);
     }
 }
