66956e5981c7eef8e28b4fb1af05b9173ea4390e
snewsome <shawnmnewsome@ufl.edu>
2017-03-15 13:03:04 -0400

    Update Hex.java

--- Src/Hex.java
+++ Src/Hex.java
@@ -3,8 +3,6 @@
  */
 
 public class Hex {
-
-    private int [] sidesConnected;
     public enum Feature{
         VOLCANO, DESERT, WATER, JUNGLE, PLAIN;
     }
@@ -12,22 +10,8 @@ public class Hex {
 
     private Feature features;
 
-    public Hex(int[] sidesConnected, Feature features) {
-        this.sidesConnected = sidesConnected;
+    public Hex(Feature features) {
         this.features = features;
-    }
-
-    public int[] getSidesConnected() {
-        return sidesConnected;
-    }
-
-    public boolean isConnected() {
-        for(int i = 0; i < sidesConnected.length; i++){
-            if(sidesConnected[i]==1){
-                return true;
-            }
-        }
-        return false;
     }
 
     public boolean hasFeatures() {
