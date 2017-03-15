35d7f07e092da5d6733cc646ccf600b6482028bd
24vincenti <vincentibarrola@Vincents-MacBook-Pro.local>
2017-03-15 17:55:11 -0400

    Player class
    Player class

---
+++ Src/Player.java
@@ -0,0 +1,18 @@
+/**
+ * Created by vincentibarrola on 3/12/17.
+ * Initialize the player
+ */
+public class Player{
+
+    private int numberOfVillagers;
+    private int numberOfTotoros;
+    private int playerScore;
+
+    //constructor for player at beginning of the game
+    public Player(){
+        numberOfVillagers = 20;
+        numberOfTotoros = 3;
+        playerScore = 0;
+    }
+
+}
