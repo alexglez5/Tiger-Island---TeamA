668dbe2e7adc88ec31c0a56ddaa1a92195857b1e
Julia <julia.woodward94@gmail.com>
2017-03-18 18:55:26 -0400

    Framework for step definition

--- src/test/java/Tigerisland/TilePlacementSteps.java
+++ src/test/java/Tigerisland/TilePlacementSteps.java
@@ -4,4 +4,22 @@ package Tigerisland;
  * Created by Alexander Gonzalez on 3/18/2017.
  */
 public class TilePlacementSteps {
+
+    @Given("^the gameboard is empty$")
+    public void the_gameboard_is_empty() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^the player places a tile$")
+    public void the_player_places_a_tile() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the volcano hex is placed at coordinate \\((\\d+),(\\d+)\\)$")
+    public void the_volcano_hex_is_placed_at_coordinate(int arg1, int arg2) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
 }
