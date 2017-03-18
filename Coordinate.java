83e3f0533815b52c1d1307ded63a66903c388534
Alexander Gonzalez <alexglez.09@ufl.edu>
2017-03-17 21:16:42 -0400

    Add files via upload

---
+++ Coordinate.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Alexander Gonzalez on 3/17/2017.
+ */
+public class Coordinate {
+    int x;
+    int y;
+
+    public Coordinate(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
