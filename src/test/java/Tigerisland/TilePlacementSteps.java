dbbaf46cf26dc2e9fcc2da5349cd3c07fb7604a0
Nathan <nepoche@yahoo.com>
2017-03-18 18:57:44 -0400

    step definition for first tile placement

--- src/test/java/Tigerisland/TilePlacementSteps.java
+++ src/test/java/Tigerisland/TilePlacementSteps.java
@@ -1,9 +1,32 @@
 package Tigerisland;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 /**
  * Created by nathanbarnavon on 3/18/17.
  */
 public class TilePlacementSteps {
 
-    
+    @Given("^the gameboard is empty$")
+    public void the_gameboard_is_empty() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^the player places a tile$")
+    public void the_player_places_a_tile() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the volcano hex is placed at coordinate \\((\\d+),(\\d+)\\)$")
+    public void the_volcano_hex_is_placed_at_coordinate(int arg1, int arg2) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
 }
