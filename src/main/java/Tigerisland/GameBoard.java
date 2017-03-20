374bab80131d49433b992cd8b0f14b7c538e3bc8
Alexander Gonzalez <alexglez.09@ufl.edu>
2017-03-20 12:11:03 -0400

    Update GameBoard to class that passes tests

--- src/main/java/Tigerisland/GameBoard.java
+++ src/main/java/Tigerisland/GameBoard.java
@@ -7,29 +7,38 @@ import java.util.Map;
  * Created by Alexander Gonzalez on 3/17/2017.
  */
 public class GameBoard {
-    private static Map<Coordinate, Hex> gameBoard = new HashMap<>();
+    protected static Map<Coordinate, Hex> gameBoard = new HashMap<>();
 
-    //Takes a tile and an array of coordinates
-    // *
-    public void placeTile(Tile tile, Coordinate [] newTileCoordinates){
-        //Assuming terrains are below volcano
-        gameBoard.put(newTileCoordinates[0],
-                tile.getLeftOfMainTerrain());
-        gameBoard.put(newTileCoordinates[1],
-                tile.getMainTerrain());
-        gameBoard.put(newTileCoordinates[2],
-                tile.getRightOfMainTerrain());
-        //Coordinate trial = gameBoard.get(tile.getMainTerrain())
+    public void placeTile(Tile tile, Coordinate mainTerrainCoordinate, int terrainsOrientation){
+        TilePlacer placer = new TilePlacer();
+        placer.placeTile(tile, mainTerrainCoordinate, terrainsOrientation);
     }
 
-    private Coordinate[] determineOrientation(){
-        //TODO based on position of two other terrains compared to volcano
-        return null;
-    }
+    //TODO delete commented code below
+//    public void placeTile(Tile tile, Orientation orientation){
+//        Coordinate [] coordinates = determineOrientation(orientation);
+//        //Assuming terrains are below volcano
+//        gameBoard.put(coordinates[0],
+//                tile.getLeftOfMainTerrain());
+//        gameBoard.put(coordinates[1],
+//                tile.getMainTerrain());
+//        gameBoard.put(coordinates[2],
+//                tile.getRightOfMainTerrain());
+//        //Coordinate trial = gameBoard.get(tile.getMainTerrain())
+//    }
 
-    public boolean checkForHex(Coordinate cord) {
-        return (gameBoard.get(cord) != null);
-    }
+//    private Coordinate[] determineOrientation(Orientation orientation){
+//        Coordinate [] coordinates = {
+//                new Coordinate(-1, 1),
+//                new Coordinate(0, 0),
+//                new Coordinate(0, 1)
+//        };
+//        return coordinates;
+//    }
+//
+//    public boolean checkForHex(Coordinate cord) {
+//        return (gameBoard.get(cord) != null);
+//    }
 
     //TODO counter to get id of tile
 }
