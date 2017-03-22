151b046b0f9c4a533c2a9abe3be5e763e5fc23f9
reedavers <reedavers@yahoo.com>
2017-03-22 16:14:58 -0400

    Player class

--- src/main/java/Tigerisland/Player.java
+++ src/main/java/Tigerisland/Player.java
@@ -3,6 +3,43 @@ package Tigerisland;
 /**
  * Created by Reed on 3/20/2017.
  */
-public enum Player {
-    black, white
+public class Player {
+    private int points;
+    int numberOfVilagersLeft;
+    int numberOfTotoroLeft;
+    int numberOfTigersLeft;
+
+    public void Player(String playerType){
+        this.numberOfVilagersLeft = 20;
+        this.numberOfTotoroLeft = 3;
+        this.numberOfTigersLeft = 2;
+    }
+
+    public int getPoints(){return this.points;}
+
+    public void addPlayerPoints(int pointsAdded){this.points += pointsAdded;}
+
+    public int getNumberOfVilagersLeft(){
+        return this.numberOfVilagersLeft;
+    }
+
+    public int getNumberOfTotoroLeft(){
+        return this.numberOfTotoroLeft;
+    }
+
+    public int getNumberOfTigersLeft(){
+        return this.numberOfTigersLeft;
+    }
+
+    public void updatePlacedVilagers(int numberOfVilagersPlaced){
+        this.numberOfVilagersLeft -= numberOfVilagersPlaced;
+    }
+
+    public void updatePlacedTotoro(int numberOfTotoroPlaced){
+        this.numberOfTotoroLeft -= numberOfTotoroPlaced;
+    }
+
+    public void updateTigersPlaced(int numberOfTigerPlaced){
+        this.numberOfTigersLeft -= numberOfTigerPlaced;
+    }
 }
