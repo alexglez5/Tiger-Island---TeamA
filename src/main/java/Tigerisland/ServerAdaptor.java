d02df562a6d75678a24f98ba4f2ac75b5972abae
Vincent Ibarrola <vibarrola@ufl.edu>
2017-04-02 17:00:56 -0400

    Created the ServerAdaptor

---
+++ src/main/java/Tigerisland/ServerAdaptor.java
@@ -0,0 +1,23 @@
+package Tigerisland;
+
+/**
+ * Created by vincentibarrola on 4/2/17.
+ */
+public class ServerAdaptor {
+    //todo
+    /*
+    - TCP Session Connection
+        - Need client socket (Waiting for port/hostname)
+            - Will read from the server
+            - WIll write out to the server
+    - Beginning code
+        - After connection is established
+            - Wait for welcome message
+            - Send password
+            - Wait for slogan
+            - Send username and password
+            - Wait for pid
+    */
+
+
+}
