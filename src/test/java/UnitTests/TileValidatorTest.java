675717bd5397dc20ba8dfcbf76c20a3014c01978
Nathan <nepoche@yahoo.com>
2017-04-02 12:38:34 -0400

    change TileValidation structure

---
+++ src/test/java/UnitTests/TileValidatorTest.java
@@ -0,0 +1,14 @@
+package UnitTests;
+
+import Tigerisland.TileValidator;
+import org.junit.Test;
+
+/**
+ * Created by nathanbarnavon on 4/2/17.
+ */
+public class TileValidatorTest {
+
+    TileValidator validator = new TileValidator();
+
+
+}
