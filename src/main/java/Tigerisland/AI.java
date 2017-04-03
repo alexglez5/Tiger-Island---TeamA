8dd4a7b441b932a16e1091c189931d45c038406f
Nathan <nepoche@yahoo.com>
2017-04-03 17:10:34 -0400

    add AI skeleton

--- src/main/java/Tigerisland/AI.java
+++ src/main/java/Tigerisland/AI.java
@@ -3,6 +3,45 @@ package Tigerisland;
 /**
  * Created by Alexander Gonzalez on 4/1/2017.
  */
+
+/* AI will be our new API and AI in one. Adaptor passes information to the AI in the following process:
+
+   <pid><place><build>
+    |
+    |
+   \/
+   <pid><tile><coordinate><orientation><build option><coordinate>(<terrain>)
+    |
+    |
+   \/
+   <boolean><tile><coordinate><orientation><build option><coordinate>(<terrain>)
+
+    if the boolean is "true", this means it's the AI's turn and thinks about the move and then moves
+    if the boolean is "false", this means the AI directly calls the move functions.
+
+    sections are delimited by spaces.
+*/
+
 public class AI {
-    GameBoard map = new GameBoard();
+    GameBoard map;
+    boolean myTurn = true;
+    Tile currentTile;
+    Coordinate tileCord;
+    Orientation orient;
+    String buildChoice;
+    Coordinate buildCord;
+    TerrainType expandArea;
+
+    public AI() {
+        map = new GameBoard();
+    }
+
+    public void placeTile(Tile tile, Coordinate c, Orientation o) {
+        map.placeTile(tile, c, o);
+    }
+
+    public void parseMessage(String message) {
+
+    }
+
 }
---
+++ src/main/test/Tigerisland/AITest.java
@@ -0,0 +1,12 @@
+package Tigerisland;
+
+import org.junit.Before;
+
+/**
+ * Created by nathanbarnavon on 4/3/17.
+ */
+public class AITest {
+
+    
+
+}
