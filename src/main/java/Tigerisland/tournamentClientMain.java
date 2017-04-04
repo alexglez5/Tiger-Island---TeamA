6a8dd880192fb3483525046105f79b3c51e2967f
Vincent Ibarrola <vibarrola@ufl.edu>
2017-04-04 15:29:03 -0400

    Server client stuff
    May need to build a server socket if he does not provide a server

--- src/main/java/Tigerisland/tournamentClientMain.java
+++ src/main/java/Tigerisland/tournamentClientMain.java
@@ -4,5 +4,39 @@ package Tigerisland;
  * Created by vincentibarrola on 4/3/17.
  */
 public class tournamentClientMain {
-    
+
+    //Will be taking in the arguments from command line based off assumption
+    public static void main(String[] args){
+        String serverIP;
+        int port;	//Doens't mean anything will get from command line
+        String tournamentPassword;
+        String username;
+        String password;
+
+        if(args.length != 5){
+            //Print to console and return to console for fixing
+            System.out.println("Invalid number of arguments to connect!!");
+            return;
+        }
+
+        serverIP = args[0];
+        port = Integer.parseInt(args[1]);
+        tournamentPassword = args[2];
+        username = args[3];
+        password = args[4];
+
+//        System.out.println(serverIP);
+//        System.out.println(port);
+//        System.out.println(tournamentPassword);
+//        System.out.println(username);
+//        System.out.println(password);
+
+        tournamentClient clientSocket = new tournamentClient(serverIP, port);
+        try{
+            clientSocket.tournamentAuthentication(tournamentPassword, username, password);
+        }catch(Exception e){
+            System.err.println("Cannot login!");
+        }
+    }
+
 }
