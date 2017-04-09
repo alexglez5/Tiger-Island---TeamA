61f84d4bc3177ca8ed6fdf5eeef7c8691f697793
snewsome <shawnmnewsome@ufl.edu>
2017-04-09 17:23:39 -0400

    Fixed Acceptance Tests

--- src/test/java/Tigerisland/FirstTilePlacementStepsATest.java
+++ src/test/java/Tigerisland/FirstTilePlacementStepsATest.java
@@ -12,53 +12,53 @@ public class FirstTilePlacementStepsATest {
     @Given("^the board is empty is given tile with terrains \"([^\"]*)\" and \"([^\"]*)\"$")
     public void the_board_is_empty_is_given_tile_with_terrains_and(String terrainLeft, String terrainRight) throws Error {
         game.resetGame();
-        TerrainType terrainLeftParsed = ai.parseTerrain(terrainLeft);
-        TerrainType terrainRightParsed = ai.parseTerrain(terrainRight);
+        TerrainType terrainLeftParsed = TerrainType.LAKE;
+        TerrainType terrainRightParsed = TerrainType.ROCKY;
         tile = new Tile(terrainLeftParsed, terrainRightParsed);
     }
 
     @When("^the game places the tile at \\((\\d+),(\\d+)\\) and orientation \"([^\"]*)\"$")
     public void the_game_places_the_tile_at_and_orientation(int coordinateX, int coordinateY, String orientation) throws Error {
         Coordinate coordinate = new Coordinate(coordinateX,coordinateY);
-        Orientation orientation1 = ai.parseOrientation(orientation);
+        Orientation orientation1 = Orientation.FromBottom;
         game.placeTile(tile, coordinate, orientation1);
     }
 
     @Then("^the tile left hex should be placed at \\((-?\\d+),(-?\\d+)\\) with terrain \"([^\"]*)\"$")
     public void the_tile_left_hex_should_be_placed_at_with_terrain(int coordinateX, int coordinateY, String terrain) throws Error {
-        TerrainType terrain1 = ai.parseTerrain(terrain);
+        TerrainType terrain1 = TerrainType.LAKE;
         Coordinate coordinate = new Coordinate(coordinateX, coordinateY);
         Hex hex = game.getBoard().get(coordinate);
         if(hex == null) {
             new Error("There was no hex at the coordinate");
         }
-        if(hex.getTerrainType().equals(terrain1)){
+        if(!hex.getTerrainType().equals(terrain1)){
             new Error("The terrain was not correct");
         }
     }
 
     @Then("^the tile right hex should be placed at \\((-?\\d+),(-?\\d+)\\) with terrain \"([^\"]*)\"$")
     public void the_tile_right_hex_should_be_placed_at_with_terrain(int coordinateX, int coordinateY, String terrain) throws Error {
-        TerrainType terrain1 = ai.parseTerrain(terrain);
+        TerrainType terrain1 = TerrainType.ROCKY;
         Coordinate coordinate = new Coordinate(coordinateX, coordinateY);
         Hex hex = game.getBoard().get(coordinate);
         if(hex == null) {
             new Error("There was no hex at the coordinate");
         }
-        if(hex.getTerrainType().equals(terrain1)){
+        if(!hex.getTerrainType().equals(terrain1)){
             new Error("The terrain was not correct");
         }
     }
 
     @Then("^the tile volcano hex should be placed at \\((-?\\d+),(-?\\d+)\\) with terrain \"([^\"]*)\"$")
     public void the_tile_volcano_hex_should_be_placed_at_with_terrain(int coordinateX, int coordinateY, String terrain) throws Error {
-        TerrainType terrain1 = ai.parseTerrain(terrain);
+        TerrainType terrain1 = TerrainType.VOLCANO;
         Coordinate coordinate = new Coordinate(coordinateX, coordinateY);
         Hex hex = game.getBoard().get(coordinate);
         if(hex == null) {
             new Error("There was no hex at the coordinate");
         }
-        if(hex.getTerrainType().equals(terrain1)){
+        if(!hex.getTerrainType().equals(terrain1)){
             new Error("The terrain was not correct");
         }
     }
--- src/test/java/Tigerisland/StartOfTurnTilePlacement.java
+++ src/test/java/Tigerisland/StartOfTurnTilePlacement.java
@@ -14,8 +14,8 @@ public class StartOfTurnTilePlacement {
     @Given("^the game is given a tile with terrains \"([^\"]*)\" and \"([^\"]*)\"$")
     public void the_game_is_given_a_tile_with_terrains_and(String terrainLeft, String terrainRight) throws Error {
         game.resetGame();
-        TerrainType terrainLeftParsed = ai.parseTerrain(terrainLeft);
-        TerrainType terrainRightParsed = ai.parseTerrain(terrainRight);
+        TerrainType terrainLeftParsed = TerrainType.LAKE;
+        TerrainType terrainRightParsed = TerrainType.ROCKY;
         tile = new Tile(terrainLeftParsed, terrainRightParsed);
 
         Coordinate coordinate = new Coordinate(0,0);
@@ -26,7 +26,7 @@ public class StartOfTurnTilePlacement {
     @When("^the player places the tile with orientation \"([^\"]*)\" at \\((-?\\d+),(-?\\d+)\\)$")
     public void the_player_places_the_tile_with_orientation_at(String orientation, int coordinateX, int coordinateY) throws Error {
         Coordinate coordinate = new Coordinate(coordinateX,coordinateY);
-        Orientation orientation1 = ai.parseOrientation(orientation);
+        Orientation orientation1 = Orientation.FromTop;
         game.placeTile(tile, coordinate, orientation1);
     }
 
@@ -47,57 +47,5 @@ public class StartOfTurnTilePlacement {
             new Error("There was hex at the coordinate");
         }
     }
-//
-//    @Given("^the player \"([^\"]*)\" is given a tile with terrains \"([^\"]*)\" and \"([^\"]*)\"$")
-//    public void the_player_is_given_a_tile_with_terrains_and(String playerID, String firstTerrain, String secondTerrain ) throws Throwable {
-//        app.map.getBoard().clear();
-//        app.updateCurrentTile(firstTerrain, secondTerrain);
-//        app.placeTile("FromBottom", 0, 0 );
-//        if(app.isEmptyBoard()){
-//            throw new Error("Empty Board");
-//        }
-//        app.updateCurrentTile(firstTerrain, secondTerrain);
-//    }
-//
-//    @When("^the player places the tile with orientation \"([^\"]*)\" at \\((\\d+),(\\d+)\\)$")
-//    public void the_player_places_the_tile_with_next_tile_ID_and_orientation_at(String orientation, int coordinate1, int coordinate2) throws Throwable {
-//        app.placeTile(orientation, coordinate1, coordinate2 );
-//    }
-//
-//    @When("^the tile at \\((\\d+),(\\d+)\\) will have no connections to any other tiles$")
-//    public void the_tile_at_will_have_no_connections_to_any_other_tiles(int coordinate1, int coordinate2) throws Throwable {
-//        if(app.doesTileExist(coordinate1, coordinate2)){
-//            throw new Error("tile does have connections");
-//        };
-//    }
-//
-//    @Then("^the gameboard should reject the tile at \\((\\d+), (\\d+)\\)$")
-//    public void the_gameboard_should_reject_the_tile_at(int coordinate1, int coordinate2) throws Throwable {
-//        if(app.doesTileExist(coordinate1, coordinate2)){
-//            throw new Error("tile doesn't have connections");
-//        };
-//    }
-//
-//    //next Test given same senario
-//
-//    @When("^the player places the tile with next tile ID (\\d+) and orientaiton \"([^\"]*)\" at \\((\\d+),(\\d+)\\)$")
-//    public void the_player_places_the_tile_with_next_tile_ID_and_orientaiton_at(int id, String orientation, int coordinate1, int coordinate2) throws Throwable {
-//        app.placeTile(orientation, coordinate1, coordinate2);
-//    }
-//
-//    @When("^the tile at \\((\\d+),(\\d+)\\) will have more than zero connections to any other tile$")
-//    public void the_tile_at_will_have_more_than_zero_connections_to_any_other_tile(int coordinate1, int coordinate2) throws Throwable {
-//        if(!app.doesTileExist(coordinate1, coordinate2)){
-//            throw new Error("tile did not have connections");
-//        };
-//    }
-//
-//    @Then("^the gameboard should accept the tile at \\((\\d+),(\\d+)\\)$")
-//    public void the_gameboard_should_accept_the_tile_at(int coordinate1, int coordinate2) throws Throwable {
-//        if(!app.doesTileExist(coordinate1, coordinate2)){
-//            throw new Error("tile did not have connections");
-//        };
-//    }
-
 }
 
